b8a1cf67126ab8e1750f2e00dff4c192efb65360
Enzo Peigné <enzo.peigne@isen-ouest.yncrea.fr>
2024-01-28 17:11:27 +0000

    Update 28/01/2024 (Some details added like footer or contact forms)

--- js/login.js
+++ js/login.js
@@ -83,3 +83,112 @@ function login(){
     }
 
 }
+
+function openPopup(type) {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'block';
+
+    $("#popupTitle").empty();
+    $("#popupText").empty();
+    
+
+    if(type == 'about'){
+        document.getElementById('popupTitle').innerText = 'A propos';
+        document.getElementById('popupText').innerText = 'TVRecap est un site web qui permet de garder une trace de tous les films et séries que vous avez regardés. Vous pouvez également proposer des contenus à ajouter au site.';
+    }
+
+    if(type == 'contact'){
+        document.getElementById('popupTitle').innerText = 'Contact';
+        // Création du formulaire de contact
+        var form = document.createElement('div');
+        form.setAttribute('id', 'contactForm');
+
+        var name = document.createElement('input');
+        name.setAttribute('type', 'text');
+        name.setAttribute('name', 'name');
+        name.setAttribute('placeholder', 'Nom');
+
+        var email = document.createElement('input');
+        email.setAttribute('type', 'email');
+        email.setAttribute('name', 'email');
+        email.setAttribute('placeholder', 'Email');
+
+
+        var message = document.createElement('textarea');
+        message.setAttribute('name', 'message');
+        message.setAttribute('placeholder', 'Message');
+    
+
+        var submit = document.createElement('button');
+        submit.setAttribute('type', 'submit');
+        submit.setAttribute('value', 'Envoyer');
+        submit.setAttribute('id', 'submit');
+        submit.setAttribute('onclick', 'submit()');
+        submit.innerText = 'Envoyer';
+
+        form.appendChild(name);
+        form.appendChild(email);
+        form.appendChild(message);
+        form.appendChild(submit);
+
+        document.getElementById('popupText').appendChild(form);
+    }
+
+
+}
+
+function closePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
+
+// Close the popup if the user clicks outside of it or presses the escape key
+window.onclick = function(event) {
+    var popup = document.getElementById('popup');
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var popup = document.getElementById('popup');
+    if (evt.keyCode == 27) {
+        popup.style.display = "none";
+    }
+};
+
+function submit(){
+    var name = $("#contactForm input[name='name']").val();
+    var email = $("#contactForm input[name='email']").val();
+    var message = $("#contactForm textarea[name='message']").val();
+
+
+    // If one of the fields is empty or the email is not valid
+    if(name == '' || email == '' || message == '' || !email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)){
+        $("#contactForm").empty();
+        $("#contactForm").append("<p>Veuillez remplir correctement tous les champs.</p>");
+        $("#contactForm").append("<button onclick=\"openPopup('contact')\">Ok</button>");
+        return;
+    }
+
+
+    $.ajax('../php/contact-form.php', {
+        method : 'POST', 
+        data : {
+            username : name,
+            userMail : email,
+            subject : 'Contact Login Page',
+            message : message
+        },
+        success: function(data){
+            if(data){
+                $("#contactForm").empty();
+                $("#contactForm").append("<p>" + data.msg + "</p>");
+            }else{
+                $("#contactForm").empty();
+                $("#contactForm").append("<p>Une erreur est survenue lors de l'envoi du message.</p>");
+            }
+        }
+    });
+}
