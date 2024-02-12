27e3efcd6c92f71d7454a5bd69b9f2c5cb4ad1c0
Enzo <71318547+epeigne@users.noreply.github.com>
2024-02-12 14:17:45 +0000

    Create constants.php template

---
+++ php/constants.php
@@ -0,0 +1,9 @@
+// Update the values with your database login informations
+
+<?php
+    const DB_USER = "user";
+    const DB_PASSWORD = "passwd";
+    const DB_NAME = "tvrecap";
+    const DB_SERVER = "127.0.0.1";
+    const DB_PORT = 3306;
+?>
