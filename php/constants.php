0a13b6c647196774a552a151445c9e0dcda902f8
Enzo Peigne <enzo.peigne@isen-ouest.yncrea.fr>
2024-02-12 15:18:33 +0100

    up

--- php/constants.php
+++
@@ -1,9 +0,0 @@
-// Update the values with your database login informations
-
-<?php
-    const DB_USER = "user";
-    const DB_PASSWORD = "passwd";
-    const DB_NAME = "tvrecap";
-    const DB_SERVER = "127.0.0.1";
-    const DB_PORT = 3306;
-?>
