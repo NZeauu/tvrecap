aaa77a645cf980e2b862de100bdeac0e9d56c410
Enzo <71318547+epeigne@users.noreply.github.com>
2024-02-12 13:59:47 +0000

    Update constants.php

--- php/constants.php
+++ php/constants.php
@@ -1,6 +1,6 @@
 <?php
-    const DB_USER = "epeigne";
-    const DB_PASSWORD = "Oku45911@";
+    const DB_USER = "dbUser";
+    const DB_PASSWORD = "passwd";
     const DB_NAME = "tvrecap";
     const DB_SERVER = "127.0.0.1";
     const DB_PORT = 3306;
